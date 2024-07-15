eef47c4ec36c8d4187fe8ff4ac4664cc90bf1bab
Leon <leon.yee000@gmail.com>
2024-07-14 18:52:38 -0700

    fixed links

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './Home.jsx'
 import Header from './Header.jsx'
