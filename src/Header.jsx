4765d96de419d27af29d13bf02b68c5028ecc1bd
Leon <leon.yee000@gmail.com>
2024-07-21 11:24:29 -0700

    universities added

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,20 @@ import Navbar from "./Navbar"
 
 export default function Header() {
   return (
-    <>
-      <div className="flex flex-row gap-4 p-4 bg-gradient-to-r from-lime-300 to-sky-400">
+    <div className="bg-gradient-to-r from-lime-300 to-sky-400">
+      <div className="flex flex-row gap-4 p-4 ">
         <Link to="/" className="w-48 md:w-1/5 object-contain">
           <img src="/CHORUS-color.png" alt="CHORUS logo" />
         </Link>
         <h1 className="hidden md:block md:text-xl lg:text-3xl font-bold text-center m-auto">Resilient Distributed CPS through Rational and Dynamic Decision-Making Among Multiple Stakeholders</h1>
       </div>
+      <div className="grid grid-flow-col w-full justify-evenly">
+        <img className="w-24 self-center" src="/universities/georgiatech.png" />
+        <img className="w-24 self-center" src="/universities/purdue.png" />
+        <img className="w-36 self-center" src="/universities/usc.png" />
+        <img className="w-24 self-center" src="/universities/wisconsin.png" />
+      </div>
       <Navbar />
-    </>
+    </div>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row border-b border-slate-400 overflow-auto">
+    <div className="flex flex-row border-y border-slate-400 overflow-auto">
       <Link to="/who-we-are" className="text-sky-600 bg-white hover:text-white hover:bg-sky-600 p-2 w-full text-center whitespace-nowrap">Who We Are</Link>
       <Link to="/" className="text-sky-600 bg-white hover:text-white hover:bg-sky-600 p-2 w-full text-center whitespace-nowrap">What We Do</Link>
       <Link to="/news" className="text-sky-600 bg-white hover:text-white hover:bg-sky-600 p-2 w-full text-center whitespace-nowrap">News & Events</Link>
