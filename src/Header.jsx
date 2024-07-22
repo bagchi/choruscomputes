87d9d2eb1403434357d9a9cb3205620e3c7ecd82
Saurabh Bagchi <sbagchi@purdue.edu>
2024-07-22 09:36:49 -0700

    Changed order of univ logos

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,8 +12,8 @@ export default function Header() {
         <h1 className="hidden md:block md:text-xl lg:text-3xl font-bold text-center m-auto">Resilient Distributed CPS through Rational and Dynamic Decision-Making Among Multiple Stakeholders</h1>
       </div>
       <div className="grid grid-flow-col w-full justify-evenly">
-        <img className="w-24 self-center" src="/universities/georgiatech.png" />
         <img className="w-24 self-center" src="/universities/purdue.png" />
+        <img className="w-24 self-center" src="/universities/georgiatech.png" />
         <img className="w-36 self-center" src="/universities/usc.png" />
         <img className="w-24 self-center" src="/universities/wisconsin.png" />
       </div>
