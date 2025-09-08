8d8d16cdad8bbf1ef55b2d55431d5af53882d0eb
Shayok Bagchi <88456623+sudocore@users.noreply.github.com>
2025-09-07 23:50:46 -0400

    Updated Josh's contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -15,7 +15,7 @@ export default function Contact() {
         <h2 className="text-2xl font-bold">Project Manager</h2>
         <p>Joshua McKerracher</p>
         <a href="mailto:jmckerra@purdue.edu" className="text-blue-600 hover:text-blue-800">jmckerra@purdue.edu</a>
-        <p>765-494-1741</p>
+        <p>765-494-3365</p>
       </div>
     </div>
   )
