ab431ba1af169534a242e512c74de9c8805beeff
Victoria Liu <viliu2000@gmail.com>
2025-09-12 15:11:26 -0400

    updated table on Education & Outreach page

--- src/Education.jsx
+++ src/Education.jsx
@@ -15,29 +15,29 @@ export default function Education() {
         <table className="border border-gray-300">
           <thead>
             <tr>
-              <th className="w-[80%]">Topic</th>
-              <th className="w-[20%]">Hours</th>
+              <th className="text-left px-4 py-2 border-b w-[80%]">Topic</th>
+              <th className="text-left px-4 py-2 border-b w-[20%]">Hours</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Introduction: Motivation, System view of dependable, design, Terminology</td>
-              <td>2</td>
+              <td className="px-4 py-2 border-b">Introduction: Motivation, System view of dependable, design, Terminology</td>
+              <td className="px-4 py-2 border-b">2</td>
             </tr>
             <tr>
-              <td>
+              <td className="px-4 py-2 border-b">
                 Big data for reliability
-                <ul>
+                <ul className="list-disc list-inside space-y-1">
                   <li>Approximate computing</li>
                   <li>Performance and failure analysis from resource usage data</li>
                   <li>Latency guarantees in Microsoft Azure cloud</li>
                   <li>Optional programming assignment: Data center clusters</li>
                 </ul>
               </td>
-              <td>3</td>
+              <td className="px-4 py-2 border-b">3</td>
             </tr>
             <tr>
-              <td>
+              <td className="px-4 py-2 border-b">
                 Big data for security
                 <ul>
                   <li>Fooling ML models</li>
@@ -46,10 +46,10 @@ export default function Education() {
                   <li>Optional programming assignment: Adversarial ML</li>
                 </ul>
               </td>
-              <td>3</td>
+              <td className="px-4 py-2 border-b">3</td>
             </tr>
             <tr>
-              <td>
+              <td className="px-4 py-2 border-b">
                 Security of distributed Machine Learning
                 <ul>
                   <li>Byzantine robust Federated Learning (FL)</li>
@@ -57,7 +57,7 @@ export default function Education() {
                   <li>Peer-to-peer and Hub-and-Spokes Learning</li>
                 </ul>
               </td>
-              <td>2</td>
+              <td className="px-4 py-2 border-b">2</td>
             </tr>
           </tbody>
         </table>
